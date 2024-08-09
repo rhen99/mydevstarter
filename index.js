3d70f5fff10f1ff9868568fbbc046cd47986fba5
Arhen Santiago <lincoldarhen9900@gmail.com>
2024-08-09 12:36:55 +0800

    added questions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 #!usr/bin/env node
+import inquirer from "inquirer";
+async function run() {
+  console.log("running...");
+  await askQuestions();
+}
+
+async function askQuestions() {
+  const whatStarter = await inquirer.prompt([
+    {
+      type: "list",
+      name: "starter",
+      choices: ["HTML & CSS", "React App"],
+      message: "What starter setup do you want?",
+    },
+  ]);
+  console.info("Answer:", whatStarter.starter);
+}
+
+function createFiles() {}
+await run();
