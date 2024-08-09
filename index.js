41e655cca87072ca5e28bca54a603f61a0a87e7a
Arhen Santiago <lincoldarhen9900@gmail.com>
2024-08-09 19:29:49 +0800

    Basic Website featue added

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!usr/bin/env node
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs/promises";
 import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+import { HTMLContent, CSSContent, JSContent } from "./contents.js";
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 async function run() {
   console.log("running...");
   await askQuestions();
@@ -10,12 +17,51 @@ async function askQuestions() {
     {
       type: "list",
       name: "starter",
-      choices: ["HTML & CSS", "React App"],
+      choices: ["Basic Website", "React App"],
       message: "What starter setup do you want?",
     },
   ]);
-  console.info("Answer:", whatStarter.starter);
+  switch (whatStarter.starter) {
+    case "Basic Website":
+      await generateBasicWebsite();
+      break;
+    case "React App":
+      console.log("Soon...");
+      break;
+    default:
+      console.log("Something went wrong.");
+  }
 }
-
-function createFiles() {}
+async function generateBasicWebsite() {
+  const spinner = createSpinner("Please Wait...").start({
+    color: "green",
+    text: "Generating scaffolding",
+  });
+  try {
+    await sleep();
+    await fs.mkdir(
+      path.dirname(fileURLToPath(import.meta.url)) + "/starter",
+      { recursive: true },
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    await fs.writeFile(
+      path.dirname(fileURLToPath(import.meta.url)) + "/starter/index.html",
+      HTMLContent
+    );
+    await fs.writeFile(
+      path.dirname(fileURLToPath(import.meta.url)) + "/starter/style.css",
+      CSSContent
+    );
+    await fs.writeFile(
+      path.dirname(fileURLToPath(import.meta.url)) + "/starter/main.js",
+      JSContent
+    );
+    spinner.success({ text: "Starter done." });
+  } catch (err) {
+    spinner.error({ text: "Something went wrong..." });
+    console.log(err);
+  }
+}
 await run();
